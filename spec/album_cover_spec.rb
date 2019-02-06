53197947db60989a290ce6e9d1c9be6743072821
maxwellbenton <maxwellbenton@gmail.com>
2019-02-06 11:42:29 -0500

    adds more tests, updates order to flow with HTML building process

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -10,16 +10,22 @@ RSpec.describe 'HTML Album Cover' do
         expect(title_box.children.any? {|ch| ch.name == "h2"}).to be == true, "No 'h2' tag found"
         expect(title_box.children.select {|ch| ch.name == "h2"}.first.children.first.text).to be == "HAPPY", "Your 'h2' tag should contain the text 'HAPPY'"
 
+        puts title_box.children.select {|ch| ch.name == "h4"}[1].children.first.text
+        expect(title_box.children.select {|ch| ch.name == "h4"}[1].children.first.text).to include("FROM DESPICABLE ME"), "A second 'h4' tag should contain the text 'FROM DESPICABLE ME'"
+
       end
 
 
       it 'the back section contains the correct header sizes and text content' do
         back_cover = parsed_html.search('#back').first
 
+        expect(back_cover.children.any? {|ch| ch.name == "h3"}).to be == true, "No 'h3' tag found"
+        expect(back_cover.children.select {|ch| ch.name == "h3"}[0].children.first.text).to include("FROM DESPICABLE ME"), "An 'h3' tag should contain the text 'FROM DESPICABLE ME'"
+
         expect(back_cover.children.any? {|ch| ch.name == "h1"}).to be == true, "No 'h1' tag found"
         expect(back_cover.children.select {|ch| ch.name == "h1"}.first.children.first.text).to be == "HAPPY", "The `h1` tag should include the text 'HAPPY'"
 
-        expect(back_cover.children.select {|ch| ch.name == "h3"}.length).to be >= 2, "Make sure to include both `h3` tags"
+        expect(back_cover.children.select {|ch| ch.name == "h3"}[1].children.first.text).to include("WRITTEN BY"), "A second 'h3' tag should contain the text 'WRITTEN BY' (remaining text optional)"
 
         expect(back_cover.children.any? {|ch| ch.name == "p"}).to be == true, "No 'p' tag found"
         expect(back_cover.children.select {|ch| ch.name == "p"}.first.children.first.text).to include("PUBLISHED BY"), "The 'p' tag should include the text 'PUBLISHED BY'"
@@ -27,7 +33,14 @@ RSpec.describe 'HTML Album Cover' do
         expect(back_cover.children.any? {|ch| ch.name == "span"}).to be == true, "No 'span' tag was found"
         expect(back_cover.children.select {|ch| ch.name == "span"}.first.children.first.text).to include("PHARRELLWILLIAMS.COM"), "The first span tag should include the text 'PHARRELLWILLIAMS.COM'"
 
-        expect(back_cover.children.select {|ch| ch.name == "img"}.length).to be >= 2, "Make sure to include both `img` tags"
+        expect(back_cover.children.any? {|ch| ch.name == "img"}).to be == true, "No 'img' tag was found"
+
+        puts back_cover.children.select {|ch| ch.name == "img"}[0].attributes["src"]
+        expect(back_cover.children.select {|ch| ch.name == "img"}[0].attributes["src"].value).to be == 'https://s3.amazonaws.com/learn-verified/columbia-logo.png', "First image should have its source set to 'https://s3.amazonaws.com/learn-verified/columbia-logo.png'"
+
+        expect(back_cover.children.select {|ch| ch.name == "span"}[1].children.first.text).to include("COLUMBIARECORDS.COM"), "The second span tag should include the text 'COLUMBIARECORDS.COM'"
+
+        expect(back_cover.children.select {|ch| ch.name == "img"}[1].attributes["src"].value).to be == 'https://s3.amazonaws.com/learn-verified/sony-logo.png', "First image should have its source set to 'https://s3.amazonaws.com/learn-verified/columbia-logo.png'"
 
       end
 
