cbbd1080166eed62759ac15186c34a795a171a83
Liz Burton <liz.burton147@gmail.com>
2021-03-18 08:46:10 -0400

    Correct test description

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'HTML Album Cover' do
         expect(title_box.children.any? {|ch| ch.name == "h2"}).to be == true, "No 'h2' tag found"
         expect(title_box.children.select {|ch| ch.name == "h2"}.first.children.first.text).to be == "HAPPY", "Your 'h2' tag should contain the text 'HAPPY'"
 
-        expect(title_box.children.select {|ch| ch.name == "h4"}[1].children.first.text).to include("FROM DESPICABLE ME"), "A second 'h4' tag should contain the text 'FROM DESPICABLE ME'"
+        expect(title_box.children.select {|ch| ch.name == "h4"}[1].children.first.text).to include("FROM DESPICABLE ME"), "A second 'h4' tag should contain the text 'FROM DESPICABLE ME 2'"
 
       end
 
@@ -19,7 +19,7 @@ RSpec.describe 'HTML Album Cover' do
         back_cover = parsed_html.search('#back').first
 
         expect(back_cover.children.any? {|ch| ch.name == "h3"}).to be == true, "No 'h3' tag found"
-        expect(back_cover.children.select {|ch| ch.name == "h3"}[0].children.first.text).to include("FROM DESPICABLE ME"), "An 'h3' tag should contain the text 'FROM DESPICABLE ME'"
+        expect(back_cover.children.select {|ch| ch.name == "h3"}[0].children.first.text).to include("FROM DESPICABLE ME"), "An 'h3' tag should contain the text 'FROM DESPICABLE ME 2'"
 
         expect(back_cover.children.any? {|ch| ch.name == "h1"}).to be == true, "No 'h1' tag found"
         expect(back_cover.children.select {|ch| ch.name == "h1"}.first.children.first.text).to be == "HAPPY", "The `h1` tag should include the text 'HAPPY'"
